5949c3bcff16f5284f530f05df127df519866089
Michael Shitrit <mshitrit@redhat.com>
2025-05-15 13:52:02 +0300

    convert to duration healthy delay
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- controllers/nodehealthcheck_controller.go
+++ controllers/nodehealthcheck_controller.go
@@ -157,7 +157,7 @@ func (r *NodeHealthCheckReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		return result, err
 	}
 
-	ctx = context.WithValue(ctx, resources.HealthyDelayContextKey, nhc.Spec.HealthyDelay)
+	ctx = context.WithValue(ctx, resources.HealthyDelayContextKey, nhc.Spec.HealthyDelay.Seconds())
 	resourceManager := resources.NewManager(r.Client, ctx, r.Log, r.Capabilities.HasMachineAPI, leaseManager, r.Recorder)
 
 	// always check if we need to patch status before we exit Reconcile
--- controllers/nodehealthcheck_controller_test.go
+++ controllers/nodehealthcheck_controller_test.go
@@ -1204,7 +1204,7 @@ var _ = Describe("Node Health Check CR", func() {
 		Context("with Node setup to delay healthy", func() {
 			BeforeEach(func() {
 				setupObjects(1, 2, false)
-				underTest.Spec.HealthyDelay = 3
+				underTest.Spec.HealthyDelay = metav1.Duration{Duration: time.Second * 3}
 			})
 			It("remediation shouldn't be created", func() {
 				// first call should fail, because the node gets unready in a few seconds only
